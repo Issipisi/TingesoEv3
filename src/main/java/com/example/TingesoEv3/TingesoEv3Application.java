200399fdaf8f699a4c2dca2dadc5d076500325ce
Isidora Rojas <88259598+Issipisi@users.noreply.github.com>
2023-12-12 01:22:00 -0300

    Nuevo repo creación proyecto

---
+++ src/main/java/com/example/TingesoEv3/TingesoEv3Application.java
@@ -0,0 +1,13 @@
+package com.example.TingesoEv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingesoEv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TingesoEv3Application.class, args);
+	}
+
+}
